9e9b99e77b62644f6d6cb5dbe8c6aaf54e6feaeb
jgs <wjdrltjr5@naver.com>
2026-01-13 17:19:52 +0900

    project : init

---
+++ src/main/java/org/onestock/OnestockApplication.java
@@ -0,0 +1,13 @@
+package org.onestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnestockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnestockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/onestock/OnestockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnestockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
